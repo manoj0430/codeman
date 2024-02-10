5b24e4ad5c9fd2f5a4cbaca2ae01c0334d62dad7
manasa lingabathula <manojl0430@gmail.com>
2024-02-10 19:07:23 +0530

    Delete a Post

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -3,18 +3,25 @@ const Post = require('../models/post');
 
 module.exports.create = async function(req,res){
     //We need to check whether post is present or not, the only we need to add a comment
-    const post= await Post.findById(req.body.post);
 
-    if(post){
-        const comment = await Comment.create({
-            content: req.body.content,
-            post: req.body.post,
-            user: req.user._id
-        });
+    try{
+        const post= await Post.findById(req.body.post);
 
-        post.comments.push(comment);
-        post.save();
-
-        res.redirect('/');
+        if(post){
+            const comment = await Comment.create({
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id
+            });
+    
+            post.comments.push(comment);
+            post.save();
+    
+            res.redirect('/');
+        }
+    }catch(err){
+        console.log('Error in fetching comments', err);
+        return;
     }
+    
 }
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const Comment = require('../models/comment');
 
 module.exports.create = async function (req, res) {
   try {
@@ -8,7 +9,26 @@ module.exports.create = async function (req, res) {
     });
     return res.redirect("back");
   } catch (err) {
-    console.log("Error in creating post");
+    console.log("Error in creating post", err);
     return;
   }
 };
+
+//action for deleting post and its related comments
+
+module.exports.destroy = async function(req,res){
+  //check whether post present or not in DB
+   try{
+    const post= await Post.findById(req.params.id);
+    //check whether the user who created the post is deleting the post
+    if(post.user == req.user.id){
+      await post.deleteOne();
+      await Comment.deleteMany({post: req.params.id})
+      return res.redirect('back');
+    }
+   }catch(err){
+    console.log('Error in Deleting user:', err);
+    return res.redirect('back');
+   }
+
+}
