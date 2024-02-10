aa4e559713068d957dbf36c6562ca6076a26688b
manasa lingabathula <manojl0430@gmail.com>
2024-02-10 23:06:46 +0530

    Create Flash messages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,106 +1,108 @@
-const User = require("../models/user");
-module.exports.profile = async function (req, res) {
-  const user = await User.findById(req.params.id);
+  const User = require("../models/user");
+  module.exports.profile = async function (req, res) {
+    const user = await User.findById(req.params.id);
 
-  return res.render("user_profile", {
-    title: "User Profile",
-    profile_user: user
-  });
-};
+    return res.render("user_profile", {
+      title: "User Profile",
+      profile_user: user
+    });
+  };
 
-//update
-module.exports.update = async function(req,res){
-  if(req.user.id == req.params.id){
-    await User.findByIdAndUpdate(req.params.id, req.body);
+  //update
+  module.exports.update = async function(req,res){
+    if(req.user.id == req.params.id){
+      await User.findByIdAndUpdate(req.params.id, req.body);
 
-    return res.redirect('back');
-  }else{
-    return res.status(401).send('UnAuthorized');
+      return res.redirect('back');
+    }else{
+      return res.status(401).send('UnAuthorized');
+    }
   }
-}
 
-// Render SignUp page
+  // Render SignUp page
 
-module.exports.signUp = function (req, res) {
-  if (req.isAuthenticated()) {
-    return res.redirect("/users/profile");
-  }
-  return res.render("user_sign_up", {
-    title: "Codeial|| SignUp",
-  });
-};
+  module.exports.signUp = function (req, res) {
+    if (req.isAuthenticated()) {
+      return res.redirect("/users/profile");
+    }
+    return res.render("user_sign_up", {
+      title: "Codeial|| SignUp",
+    });
+  };
 
-// Render SignIn page
+  // Render SignIn page
 
-module.exports.signIn = function (req, res) {
-  if (req.isAuthenticated()) {
-    return res.redirect("/users/profile");
-  }
-  return res.render("user_sign_in", {
-    title: "Codeial || SignIn",
-  });
-};
+  module.exports.signIn = function (req, res) {
+    if (req.isAuthenticated()) {
+      return res.redirect("/users/profile");
+    }
+    return res.render("user_sign_in", {
+      title: "Codeial || SignIn",
+    });
+  };
 
-//get Sign Up Data
+  //get Sign Up Data
 
-// module.exports.create= function(req,res){
-//     //check whether password and confirm password are same
-//     if(req.body.password != req.body.confirm_password){
-//       return res.redirect('back');
-//     }
+  // module.exports.create= function(req,res){
+  //     //check whether password and confirm password are same
+  //     if(req.body.password != req.body.confirm_password){
+  //       return res.redirect('back');
+  //     }
 
-//     //check whether email already exits or not
-//     User.findOne({email: req.body.email}, function(err,user){
-//       if(err){console.log("Error in finding the user", err); return;}
+  //     //check whether email already exits or not
+  //     User.findOne({email: req.body.email}, function(err,user){
+  //       if(err){console.log("Error in finding the user", err); return;}
 
-//         if(!user){
-//           User.create(req.body, function(err,user){
-//             if(err){console.log("Error in creating user while signing up", err); return;}
+  //         if(!user){
+  //           User.create(req.body, function(err,user){
+  //             if(err){console.log("Error in creating user while signing up", err); return;}
 
-//             return res.redirect('/users/signin');
-//           })
-//         }else{
-//           return res.direct('back');
-//         }
+  //             return res.redirect('/users/signin');
+  //           })
+  //         }else{
+  //           return res.direct('back');
+  //         }
 
-//     })
+  //     })
 
-// }
+  // }
 
-module.exports.create = async function (req, res) {
-  //check whether password and confirm password are same
-  if (req.body.password != req.body.confirm_password) {
-    return res.redirect("back");
-  }
-  try {
-    // Check whether email id exits or not
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      await User.create(req.body);
-
-      return res.redirect("/users/signin");
-    } else {
+  module.exports.create = async function (req, res) {
+    //check whether password and confirm password are same
+    if (req.body.password != req.body.confirm_password) {
       return res.redirect("back");
     }
-  } catch (err) {
-    console.log("error in creating/sign up the user", err);
-    return;
+    try {
+      // Check whether email id exits or not
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        await User.create(req.body);
+
+        return res.redirect("/users/signin");
+      } else {
+        return res.redirect("back");
+      }
+    } catch (err) {
+      console.log("error in creating/sign up the user", err);
+      return;
+    }
+  };
+
+  // To sign in and create a session
+  module.exports.createSession = function (req, res) {
+    req.flash('success','Logged in Sucessfully!!');
+    return res.redirect("/");
+  };
+
+
+  //signout
+
+  module.exports.destroySession = function(req,res){
+    //first logout
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      req.flash('success', 'Successfully Logged off!!');
+      res.redirect('/');
+    });
   }
-};
-
-// To sign in and create a session
-module.exports.createSession = function (req, res) {
-  return res.redirect("/");
-};
-
-
-//signout
-
-module.exports.destroySession = function(req,res){
-  //first logout
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
-  });
-}
 
