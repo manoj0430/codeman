7c0eebb00dc82bcb241411b4c095c42392ae2a88
manasa lingabathula <manojl0430@gmail.com>
2024-02-10 15:19:39 +0530

    Display Posts and Users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,18 @@
-module.exports.home = function(req,res){
+const Post = require('../models/post');
+module.exports.home = async function(req,res){
+    try{
+        const posts = await Post.find({}).populate('user').exec();
+        
+        return res.render('home',{
+            title: 'Codeial || Posts',
+            posts: posts
+        })
+
+    }catch(err){
+        console.log("Error in fetching posts", err);
+        return;
+    }
     
-    return res.render('home',{
-        title: "Home"
-    }); 
 }
 
 
--- models/post.js
+++ models/post.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 
-const postSchema = new mongoose({
+const postSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
-
+        ref: 'User'
     }
 },{
     timestamps: true
 });
+
 
 const Post = mongoose.model('Post', postSchema);
 
